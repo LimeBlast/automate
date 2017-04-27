eeb53c80825dca05f8bab4d40dc4f1a9417407b0
Daniel Hollands <daniel@limeblast.co.uk>
2017-04-27 19:55:35 +0100

    added armchair and desk. This code needs a serious refactor

--- dash.js
+++ dash.js
@@ -2,9 +2,11 @@ var dash_button = require('node-dash-button');
 var spawn = require("child_process").spawn;
 
 var goodnight = "ac:63:be:39:43:68", // Nescafe Dolce Gusto
-    lunch = "ac:63:be:7c:60:90"; // Biona Organic
+    lunch = "ac:63:be:7c:60:90", // Biona Organic
+    armchair = "ac:63:be:7b:85:8c", // Right Guard
+    desk = "0c:47:c9:e7:46:e5"; // Fiesta kitchen towel
 
-var buttons = [goodnight, lunch];
+var buttons = [goodnight, lunch, armchair, desk];
 
 var dash = dash_button(buttons, null, null, 'all'); //address from step above
 
@@ -17,6 +19,12 @@ dash.on("detected", function (dash_id) {
     } else if (dash_id === lunch) {
         console.log("lunch was pressed");
         process = spawn('python3', ["/home/pi/Code/automate/lunch.py"]);
+    } else if (dash_id === armchair) {
+        console.log("armchair was pressed");
+        process = spawn('python3', ["/home/pi/Code/automate/armchair.py"]);
+    } else if (dash_id === desk) {
+        console.log("desk was pressed");
+        process = spawn('python3', ["/home/pi/Code/automate/desk.py"]);
     }
 
     // http://stackoverflow.com/a/23452742/1049688
