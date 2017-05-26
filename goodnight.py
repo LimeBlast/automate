1952dc03ee130e49fce9281fffa62d991fd92388
Daniel Hollands <daniel@limeblast.co.uk>
2017-05-26 19:20:44 +0100

    Update python syntax

--- goodnight.py
+++ goodnight.py
@@ -3,49 +3,55 @@ import sys
 
 import soco
 
-# examples taken from https://github.com/SoCo/SoCo/blob/master/examples/commandline/tunein.py
-tunein_service = 'SA_RINCON65031_'
-meta_template = """
-<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/"
-    xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"
-    xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/"
-    xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/">
-    <item id="R:0/0/0" parentID="R:0/0" restricted="true">
-        <dc:title>{title}</dc:title>
-        <upnp:class>object.item.audioItem.audioBroadcast</upnp:class>
-        <desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">
-            {service}
-        </desc>
-    </item>
-</DIDL-Lite>' """
-uri = 'x-sonosapi-stream:s51002?sid=254&flags=8224&sn=0'
-uri = uri.replace('&', '&amp;')
-meta = meta_template.format(title="Drone Zone", service=tunein_service)
-two_hours = 7200
 
-for device in soco.discover():
-    if device.player_name == 'Master Bedroom':
-        break
-    else:
-        device = None
+def main():
+    # examples taken from https://github.com/SoCo/SoCo/blob/master/examples/commandline/tunein.py
+    tunein_service = 'SA_RINCON65031_'
+    meta_template = """
+    <DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"
+        xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/"
+        xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/">
+        <item id="R:0/0/0" parentID="R:0/0" restricted="true">
+            <dc:title>{title}</dc:title>
+            <upnp:class>object.item.audioItem.audioBroadcast</upnp:class>
+            <desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">
+                {service}
+            </desc>
+        </item>
+    </DIDL-Lite>' """
+    uri = 'x-sonosapi-stream:s51002?sid=254&flags=8224&sn=0'
+    uri = uri.replace('&', '&amp;')
+    meta = meta_template.format(title="Drone Zone", service=tunein_service)
+    two_hours = 7200
 
-print("Controlling {}".format(device.player_name))
+    for device in soco.discover():
+        if device.player_name == 'Master Bedroom':
+            break
+        else:
+            device = None
 
-print("Leaving any groups")
-device.unjoin()
+    print("Controlling {}".format(device.player_name))
 
-print("Setting volume")
-device.volume = 25
+    print("Leaving any groups")
+    device.unjoin()
 
-print("Saying goodnight")
-device.play_uri('https://raw.githubusercontent.com/LimeBlast/automate/master/goodnight.mp3')
-time.sleep(3)
+    print("Setting volume")
+    device.volume = 25
 
-print("Playing Drone Zone")
-device.play_uri(uri, meta)
+    print("Saying goodnight")
+    device.play_uri('https://raw.githubusercontent.com/LimeBlast/automate/master/goodnight.mp3')
+    time.sleep(3)
 
-print("Setting sleep timer")
-device.set_sleep_timer(two_hours)
+    print("Playing Drone Zone")
+    device.play_uri(uri, meta)
 
-# http://stackoverflow.com/a/23452742/1049688
-sys.stdout.flush()
+    print("Setting sleep timer")
+    device.set_sleep_timer(two_hours)
+
+    # http://stackoverflow.com/a/23452742/1049688
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main()
--- lunch.py
+++ lunch.py
@@ -5,18 +5,24 @@ from slackclient import SlackClient
 
 import settings
 
-sc = SlackClient(settings.SLACK_API_TOKEN_FOXSOFT)
 
-print("Setting status")
-result = sc.api_call(
-    "users.profile.set",
-    user=settings.SLACK_USER_FOXSOFT,
-    profile=json.dumps({
-        "status_text": "Having lunch",
-        "status_emoji": ":fork_and_knife:"
-    })
-)
-print(result)
+def main():
+    sc = SlackClient(settings.SLACK_API_TOKEN_FOXSOFT)
 
-# http://stackoverflow.com/a/23452742/1049688
-sys.stdout.flush()
+    print("Setting status")
+    result = sc.api_call(
+        "users.profile.set",
+        user=settings.SLACK_USER_FOXSOFT,
+        profile=json.dumps({
+            "status_text": "Having lunch",
+            "status_emoji": ":fork_and_knife:"
+        })
+    )
+    print(result)
+
+    # http://stackoverflow.com/a/23452742/1049688
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main()
